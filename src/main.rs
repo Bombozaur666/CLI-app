5f638e4f200a80f4ddd7f5918551a885db59df4f
Bombozaur666 <bombozaur6@gmail.com>
2024-03-14 00:50:08 +0100

    cosmetic changes

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::io;
 use reqwest;
 use serde_json::Value;
 static CURRENCY_CODES: [&str; 4]=  ["pln", "usd", "eur", "fjd"];
-fn request_manager(source:&str, destiny:&str, quantity:&str) {
+fn request_manager(source: &str, destiny: &str, quantity: &str) {
     let quantity: f64 = quantity.parse::<f64>().unwrap();
     if CURRENCY_CODES.contains(&source) && CURRENCY_CODES.contains(&destiny) && quantity > 0.0 {
-        let url = format!("{}{}","https://open.er-api.com/v6/latest/", &source);
+        let url = "https://open.er-api.com/v6/latest/".to_owned() + source;
         match reqwest::blocking::get(url) {
             Ok(response) => {
                 if response.status().is_success() {
@@ -14,9 +14,9 @@ fn request_manager(source:&str, destiny:&str, quantity:&str) {
                             let json: Value =  serde_json::from_str(&body).unwrap();
                             let rates: f64 = json.get("rates").unwrap().get(destiny.to_uppercase()).unwrap().as_f64().unwrap();
                             if !rates.is_nan() {
-                                println!("Converted value: {} for rates: {:?}", &rates * quantity, rates);
+                                println!("Converted value: {} for rates: {}", rates * quantity, rates);
                             } else {
-                                println!("There is no currency code as: {}", &destiny);
+                                eprintln!("There is no currency code as: {}", destiny);
                             }
                         }
                         Err(err) => eprintln!("Error: {:?}", err)
@@ -28,7 +28,7 @@ fn request_manager(source:&str, destiny:&str, quantity:&str) {
             Err(err) => eprintln!("Error: {:?}", err),
         }
     } else {
-        println!("At least one of the parameters is incorrect. For all currencies type \"list\".\n\
+        eprintln!("At least one of the parameters is incorrect. For all currencies type \"list\".\n\
                   Source: {:?}\n\
                   Destiny: {:?}\n\
                   Quantity: {:?}\n",
@@ -46,7 +46,7 @@ fn main() {
             ["list"] => println!("{:?}", CURRENCY_CODES),
             ["exit"] => break,
             [source, destiny, quantity] => request_manager(source, destiny, quantity),
-            _ => println!("I don't recognize: \n{:?}", input.join(" "))
+            _ => eprintln!("I don't recognize: \n{:?}", input.join(" "))
         }
     }
 }
