91d46104ce4c89bce6097fe6be56ebbf6b35cf8f
Bombozaur666 <bombozaur6@gmail.com>
2024-03-14 00:19:37 +0100

    better type

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use reqwest;
 use serde_json::Value;
 static CURRENCY_CODES: [&str; 4]=  ["pln", "usd", "eur", "fjd"];
 fn request_manager(source:&str, destiny:&str, quantity:&str) {
-    if CURRENCY_CODES.contains(&source) && CURRENCY_CODES.contains(&destiny) && &quantity.parse::<f64>().unwrap() > &0.0 {
+    let quantity: f64 = quantity.parse::<f64>().unwrap();
+    if CURRENCY_CODES.contains(&source) && CURRENCY_CODES.contains(&destiny) && quantity > 0.0 {
         let url = format!("{}{}","https://open.er-api.com/v6/latest/", &source);
         match reqwest::blocking::get(url) {
             Ok(response) => {
@@ -13,7 +14,7 @@ fn request_manager(source:&str, destiny:&str, quantity:&str) {
                             let json: Value =  serde_json::from_str(&body).unwrap();
                             let rates: f64 = json.get("rates").unwrap().get(destiny.to_uppercase()).unwrap().as_f64().unwrap();
                             if !rates.is_nan() {
-                                println!("Converted value: {} for rates: {:?}", &rates * &quantity.parse::<f64>().unwrap(), rates);
+                                println!("Converted value: {} for rates: {:?}", &rates * quantity, rates);
                             } else {
                                 println!("There is no currency code as: {}", &destiny);
                             }
